1739955a47d91747fc6c9be54685bddd99f5c659
Federico Giugni <f.giugni@studenti.unica.it>
2025-01-19 19:31:33 +0100

    Test dell'esecuzione
    Test generale di esecuzione con esito positivo, tutti i comportamenti rispecchiano quelli attesi.
    
    Rimangono da testare le scritture agli attuatori, e il FlightController appena si riesce ad avere lettura IMU.

--- src/ESP32_AircraftFlightController.ino
+++ src/ESP32_AircraftFlightController.ino
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include "Aircraft.h"
 #include "SystemController.h"
+#include "FlightController.h"
 #include "Logger.h"
 #include "WiFiManager.h"
 
@@ -9,6 +10,8 @@ const char *ssid = "TIM-19028281";  ///< SSID della rete Wi-Fi
 const char *password = "casa12345"; ///< Password della rete Wi-Fi
 
 Aircraft *aircraft = nullptr;
+FlightController *flightController = nullptr;
+
 SystemController controller = SystemController();
 
 static unsigned long tPrev = 0;               ///< Timestamp dell'ultimo ciclo in millisecondi.
@@ -24,6 +27,8 @@ void setup()
     delay(1000);
 
     aircraft = new Aircraft();
+    flightController = new FlightController(aircraft->receiver_data, aircraft->imu_data, aircraft->output);
+
     Logger::getInstance().log(LogLevel::INFO, "Setup complete.");
 }
 
@@ -41,6 +46,9 @@ void loop()
         controller.update_state(aircraft->receiver_data);
         controller.update_modes(aircraft->receiver_data, aircraft->imu.isSetupComplete);
         controller.check_errors();
+        flightController->control(dt, aircraft->imu_data, aircraft->receiver_data, aircraft->output, controller.assist_mode, controller.state, controller.calibration_target);
+        controller.set_output(aircraft->output, aircraft->receiver_data, aircraft->imu.isSetupComplete);
         aircraft->update_leds(controller.assist_mode, controller.state);
+        aircraft->write_actuators();
     }
 }
